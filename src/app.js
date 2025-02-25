8d804c8711d5f752b571da8737a66772087179cf
DharmikAmbaliya5448 <dharmikambaliya153@gmail.com>
2025-02-25 10:17:19 +0530

    Handles Multiple Routes

--- src/app.js
+++ src/app.js
@@ -17,18 +17,47 @@ app.get("/test", (req, res) => {
 // });
 
 app.get(/.*fly$/, (req, res) => {
-    // console.log(req.query)
+  // console.log(req.query)
   res.send({ firstname: "Dharmik", lastname: "Ambaliya" });
 });
 
-app.post("/user",(req,res) => {
-    res.send("Data send successfully");
+app.post("/user", (req, res) => {
+  res.send("Data send successfully");
 });
- 
+
 app.get("/hello", (req, res) => {
   res.send("Hello hello hello!!!");
 });
 
+app.get(
+  "/hii",
+  [(req, res, next) => {
+    console.log("Handling the route user!!!");
+    //res.send("Response!!!");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling Route 2nd!!!");
+    //res.send("2ns Route...");
+    next();
+  }],
+  (req, res, next) => {
+    console.log("Handling the 3rd Route");
+    //res.send("3rd Route");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Handling the 4th Route");
+    res.send("4th Route");
+    next();
+  }
+);
+
 app.listen(7777, () => {
   console.log("Surver is running on port 7777.");
 });
+
+//Array inside route
+// app.use("/routes",rH1,rH2,rH3,rH4,rH5);
+// app.use("/routes",[rH1,rH2,rH3,rH4,rH5]);
+// app.use("/routes",rH1,[rH2,rH3],rH4,rH5);
