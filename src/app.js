73007f3da8dcff35a0df8f8e24eae39f1214a235
DharmikAmbaliya5448 <dharmikambaliya153@gmail.com>
2025-05-30 16:05:58 +0530

    Update the back-end

--- src/app.js
+++ src/app.js
@@ -14,10 +14,10 @@ app.use(
   })
 );
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+// app.use(cors({
+//   origin: "http://localhost:5173",
+//   credentials: true,
+// }));
 
 app.use(express.json());
 app.use(cookieParser());
